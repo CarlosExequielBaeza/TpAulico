7a774c2a05b16d0ef39c0de6b4b12cb9d2b6830b
Pablo Damián Alcaraz <pablodalcaraz@gmail.com>
2024-03-22 16:32:35 -0300

    getter setter class Carta

--- TpAul/src/main/java/com/mycompany/tpaul/carta.java
+++ TpAul/src/main/java/com/mycompany/tpaul/carta.java
@@ -8,6 +8,23 @@ public class carta {
         this.numero = numero;
         this.palo = palo;
     }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getPalo() {
+        return palo;
+    }
+
+    public void setPalo(String palo) {
+        this.palo = palo;
+    }
+    
     
     @Override
     public String toString(){
